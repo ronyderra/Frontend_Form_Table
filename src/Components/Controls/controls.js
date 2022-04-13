70f63a76b384b8ad53c7fbbe336ecc671a4748a8
ronyderra <derrarony@gmail.com>
2022-04-13 21:07:34 +0300

    export controlers

---
+++ src/Components/Controls/controls.js
@@ -0,0 +1,14 @@
+import Input from "./input";
+import Select from "./select";
+import DatePicker from "./datePicker";
+import Button from "./button";
+
+
+const Controls = {
+    Input,
+    Select,
+    DatePicker,
+    Button,
+}
+
+export default Controls;
