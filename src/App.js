060e42d8963d15dddcf6d8c0af0b951cad174ad0
ronyderra <derrarony@gmail.com>
2022-04-13 21:09:55 +0300

    ignore

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import MainForm from './Components/Form/mainForm'
 
 function App() {
   return (
     <div className="App">
+      <MainForm />
     </div>
   );
 }
