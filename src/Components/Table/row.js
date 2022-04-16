ec34a8a82264603b39f68f02d7adae1d669787d1
Rony Derra <derrarony@gmail.com>
2022-04-16 17:53:31 +0300

    row css

--- src/Components/Table/row.js
+++ src/Components/Table/row.js
@@ -9,14 +9,14 @@ const Row = ({ data }) => {
                 data.map((row, key) => (
                     <TableRow key={key} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
                         <TableCell component="th" scope="row">{row.id}</TableCell>
-                        <TableCell align="right">{row.nameH}</TableCell>
-                        <TableCell align="right">{row.nameE}</TableCell>
-                        <TableCell align="right">{row.birthDate}</TableCell>
-                        <TableCell align="right">{row.personalId}</TableCell>
-                        <TableCell align="right">{row.city}</TableCell>
-                        <TableCell align="right">{row.bank}</TableCell>
-                        <TableCell align="right">{row.branch}</TableCell>
-                        <TableCell align="right">{row.accountNumber}</TableCell>
+                        <TableCell align="center">{row.nameH}</TableCell>
+                        <TableCell align="center">{row.nameE}</TableCell>
+                        <TableCell align="center">{row.birthDate}</TableCell>
+                        <TableCell align="center">{row.personalId}</TableCell>
+                        <TableCell align="center">{row.city}</TableCell>
+                        <TableCell align="center">{row.bank}</TableCell>
+                        <TableCell align="center">{row.branch}</TableCell>
+                        <TableCell align="center">{row.accountNumber}</TableCell>
                     </TableRow>
                 ))
             }
