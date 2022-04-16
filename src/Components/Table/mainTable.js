4d4d9af15749dac0b433cd599d4ff6d2d15500c2
Rony Derra <derrarony@gmail.com>
2022-04-16 17:53:39 +0300

    table

--- src/Components/Table/mainTable.js
+++ src/Components/Table/mainTable.js
@@ -11,26 +11,13 @@ import axios from 'axios';
 import ColumnData from './columnData'
 import { Link } from "react-router-dom";
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
 const MainTable = () => {
 
   const [rowsData, setRowsData] = useState([{}])
 
   useEffect(() => {
     let data = '';
-
-    var config = {
+    const config = {
       method: 'get',
       url: 'https://localhost:7155/api/UserList',
       headers: {},
@@ -39,7 +26,6 @@ const MainTable = () => {
 
     axios(config)
       .then(function (response) {
-        console.log(JSON.stringify(response.data));
         setRowsData(response.data)
       })
       .catch(function (error) {
